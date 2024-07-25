7c122c2563db734ecfcc5b8c8238355b323cdca8
chuinga <mfproencamartins@gmail.com>
2024-07-25 15:40:20 +0100

    feat: connect contact form to EmailJS, still with bugs

---
+++ scripts/form.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+    emailjs.init("eWRRuKVf9FmfR9nJN");
+
+    document.getElementById('form').addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        emailjs.sendForm('service_0uz2zl7', 'template_omyvadb', this)
+            .then(function() {
+                alert('Email sent successfully!');
+            }, function(error) {
+                alert('Failed to send email. Error: ' + JSON.stringify(error));
+            });
+    });
+});
+
