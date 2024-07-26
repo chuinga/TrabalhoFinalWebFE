8deca6a62b5b3dbbcdd68b9222e313d6698e7c69
Miguel Martins <mfproencamartins@gmail.com>
2024-07-26 02:36:48 +0100

    feat: add questions to quiz game

--- scripts/quiz-pt.js
+++ scripts/quiz-pt.js
@@ -108,7 +108,184 @@ function finishGame() {
     </button>`;
 }
 
-// Questions array containing the quiz questions and answers
+// Array de perguntas contendo as perguntas e respostas do quiz
+const perguntas = [
+  // Perguntas de escolha múltipla
+  {
+    pergunta: "Como se chama a praça principal de Setúbal?",
+    respostas: [
+      { texto: "Praça da Liberdade", correcto: false },
+      { texto: "Praça da República", correcto: false },
+      { texto: "Praça do Bocage", correcto: true },
+      { texto: "Praça do Comércio", correcto: false },
+    ],
+  },
+  {
+    pergunta: "Qual famoso poeta português é comemorado com uma estátua em Setúbal?",
+    respostas: [
+      { texto: "Manuel Maria Barbosa du Bocage", correcto: true },
+      { texto: "Luís de Camões", correcto: false },
+      { texto: "Fernando Pessoa", correcto: false },
+      { texto: "Antero de Quental", correcto: false },
+    ],
+  },
+  {
+    pergunta: "Qual é o nome do rio que atravessa Setúbal?",
+    respostas: [
+      { texto: "Douro", correcto: false },
+      { texto: "Mondego", correcto: false },
+      { texto: "Sado", correcto: true },
+      { texto: "Tejo", correcto: false },
+    ],
+  },
+  {
+    pergunta: "Qual edifício histórico em Setúbal foi outrora um mosteiro jesuíta?",
+    respostas: [
+      { texto: "Convento de Jesus", correcto: true },
+      { texto: "Convento de São Francisco", correcto: false },
+      { texto: "Convento de Santa Clara", correcto: false },
+      { texto: "Convento do Carmo", correcto: false },
+    ],
+  },
+  {
+    pergunta: "Qual ponte de Setúbal liga a cidade à Península de Troia?",
+    respostas: [
+      { texto: "Ponte 25 de Abril", correcto: false },
+      { texto: "Ponte Vasco da Gama", correcto: false },
+      { texto: "Ponte de São Francisco", correcto: false },
+      { texto: "Ponte da Doca", correcto: true },
+    ],
+  },
+  {
+    pergunta: "Qual é o pico mais alto da Serra da Arrábida?",
+    respostas: [
+      { texto: "Serra do Risco", correcto: false },
+      { texto: "Serra da Arrábida", correcto: true },
+      { texto: "Serra da Estrela", correcto: false },
+      { texto: "Serra de Sintra", correcto: false },
+    ],
+  },
+  {
+    pergunta: "Qual flora única é comum no Parque Natural da Arrábida?",
+    respostas: [
+      { texto: "Pinheiros", correcto: false },
+      { texto: "Sobreiros", correcto: false },
+      { texto: "Mato mediterrânico", correcto: true },
+      { texto: "Oliveiras", correcto: false },
+    ],
+  },
+  {
+    pergunta: "Qual sítio histórico está localizado na Serra da Arrábida?",
+    respostas: [
+      { texto: "Convento de Cristo", correcto: false },
+      { texto: "Convento da Arrábida", correcto: true },
+      { texto: "Convento de Mafra", correcto: false },
+      { texto: "Convento de Belém", correcto: false },
+    ],
+  },
+  {
+    pergunta: "Pelo que é mais conhecido o Parque Natural da Arrábida?",
+    respostas: [
+      { texto: "Falésias costeiras e biodiversidade", correcto: true },
+      { texto: "Paisagens desérticas", correcto: false },
+      { texto: "Desportos de neve", correcto: false },
+      { texto: "Actividade vulcânica", correcto: false },
+    ],
+  },
+  {
+    pergunta: "Qual actividade ao ar livre é comum na Serra da Arrábida?",
+    respostas: [
+      { texto: "Surf", correcto: false },
+      { texto: "Escalada", correcto: true },
+      { texto: "Esqui", correcto: false },
+      { texto: "Parapente", correcto: false },
+    ],
+  },
+  {
+    pergunta: "Qual praia é conhecida pelas suas águas límpidas e está localizada na Arrábida?",
+    respostas: [
+      { texto: "Praia da Rocha", correcto: false },
+      { texto: "Praia da Figueirinha", correcto: true },
+      { texto: "Praia de Carcavelos", correcto: false },
+      { texto: "Praia da Nazaré", correcto: false },
+    ],
+  },
+  {
+    pergunta: "Qual praia é famosa pela sua vista da Península de Troia?",
+    respostas: [
+      { texto: "Praia da Adraga", correcto: false },
+      { texto: "Praia da Ribeira", correcto: false },
+      { texto: "Praia de Galápos", correcto: true },
+      { texto: "Praia da Manta Rota", correcto: false },
+    ],
+  },
+  {
+    pergunta: "Qual é uma actividade comum na Praia dos Coelhos?",
+    respostas: [
+      { texto: "Mergulho", correcto: true },
+      { texto: "Snowboard", correcto: false },
+      { texto: "Windsurf", correcto: false },
+      { texto: "Pesca", correcto: false },
+    ],
+  },
+  {
+    pergunta: "Qual praia é conhecida pelo seu ambiente isolado e tranquilo na Arrábida?",
+    respostas: [
+      { texto: "Praia da Comporta", correcto: false },
+      { texto: "Praia de Albarquel", correcto: true },
+      { texto: "Praia do Guincho", correcto: false },
+      { texto: "Praia da Amoreira", correcto: false },
+    ],
+  },
+  {
+    pergunta: "Qual praia faz parte do Parque Natural da Arrábida e tem acesso limitado?",
+    respostas: [
+      { texto: "Praia da Rocha", correcto: false },
+      { texto: "Praia do Portinho da Arrábida", correcto: true },
+      { texto: "Praia da Foz do Arelho", correcto: false },
+      { texto: "Praia de São Pedro de Moel", correcto: false },
+    ],
+  },
+  // Perguntas de verdadeiro ou falso
+  {
+    pergunta: "O Choco Frito é um prato tradicional de Setúbal?",
+    respostas: [
+      { texto: "Verdadeiro", correcto: true },
+      { texto: "Falso", correcto: false },
+    ],
+  },
+  {
+    pergunta: "As Tortas de Azeitão são um tipo de queijo?",
+    respostas: [
+      { texto: "Falso", correcto: true },
+      { texto: "Verdadeiro", correcto: false },
+    ],
+  },
+  {
+    pergunta: "O Queijo de Azeitão é um queijo suave e cremoso feito de leite de vaca?",
+    respostas: [
+      { texto: "Falso", correcto: true },
+      { texto: "Verdadeiro", correcto: false },
+    ],
+  },
+  {
+    pergunta: "O Moscatel de Setúbal é um vinho doce e fortificado feito de uvas Muscat?",
+    respostas: [
+      { texto: "Verdadeiro", correcto: true },
+      { texto: "Falso", correcto: false },
+    ],
+  },
+  {
+    pergunta: "As Sardinhas Assadas são um prato comum em Setúbal?",
+    respostas: [
+      { texto: "Verdadeiro", correcto: true },
+      { texto: "Falso", correcto: false },
+    ],
+  },
+];
+
+
+/* // Questions array containing the quiz questions and answers
 const questions = [
   {
     pergunta: "Como se chama a praça principal de Setúbal?",
@@ -154,3 +331,4 @@ const questions = [
     ],
   },
 ];
+ */
--- scripts/quiz.js
+++ scripts/quiz.js
@@ -87,6 +87,35 @@ function finishGame() {
 
   switch (true) {
     case performance >= 90:
+      message = "Excellent!";
+      break;
+    case performance >= 70:
+      message = "Very good!";
+      break;
+    case performance >= 50:
+      message = "Good!";
+      break;
+    default:
+      message = "Needs improvement.";
+  }
+
+  $questionsContainer.innerHTML = `<p class="final-message">
+      You got ${totalcorreto} out of ${totalQuestions} questions right!
+      <span>Result: ${message}</span>
+    </p>
+    <button onclick=window.location.reload() class="button">
+      Retake the test
+    </button>`;
+}
+
+/* function finishGame() {
+  const totalQuestions = questions.length;
+  const performance = Math.floor((totalcorreto * 100) / totalQuestions);
+
+  let message = "";
+
+  switch (true) {
+    case performance >= 90:
       message = "Excelente!";
       break;
     case performance >= 70:
@@ -106,9 +135,186 @@ function finishGame() {
     <button onclick=window.location.reload() class="button">
       Repetir o teste
     </button>`;
-}
+} */
 
 // Questions array containing the quiz questions and answers
+const questions = [
+  // Multiple-choice questions
+  {
+    pergunta: "What is the main square of Setúbal called?",
+    respostas: [
+      { text: "Praça da Liberdade", correto: false },
+      { text: "Praça da República", correto: false },
+      { text: "Praça do Bocage", correto: true },
+      { text: "Praça do Comércio", correto: false },
+    ],
+  },
+  {
+    pergunta: "Which famous Portuguese poet is commemorated with a statue in Setúbal?",
+    respostas: [
+      { text: "Manuel Maria Barbosa du Bocage", correto: true },
+      { text: "Luís de Camões", correto: false },
+      { text: "Fernando Pessoa", correto: false },
+      { text: "Antero de Quental", correto: false },
+    ],
+  },
+  {
+    pergunta: "What is the name of the river that flows through Setúbal?",
+    respostas: [
+      { text: "Douro", correto: false },
+      { text: "Mondego", correto: false },
+      { text: "Sado", correto: true },
+      { text: "Tejo", correto: false },
+    ],
+  },
+  {
+    pergunta: "Which historical building in Setúbal was once a Jesuit monastery?",
+    respostas: [
+      { text: "Convento de Jesus", correto: true },
+      { text: "Convento de São Francisco", correto: false },
+      { text: "Convento de Santa Clara", correto: false },
+      { text: "Convento do Carmo", correto: false },
+    ],
+  },
+  {
+    pergunta: "Which Setúbal bridge connects the city to the Troia Peninsula?",
+    respostas: [
+      { text: "Ponte 25 de Abril", correto: false },
+      { text: "Ponte Vasco da Gama", correto: false },
+      { text: "Ponte de São Francisco", correto: false },
+      { text: "Ponte da Doca", correto: true },
+    ],
+  },
+  {
+    pergunta: "What is the highest peak in the Arrábida mountains?",
+    respostas: [
+      { text: "Serra do Risco", correto: false },
+      { text: "Serra da Arrábida", correto: true },
+      { text: "Serra da Estrela", correto: false },
+      { text: "Serra de Sintra", correto: false },
+    ],
+  },
+  {
+    pergunta: "Which unique flora is commonly found in the Arrábida Natural Park?",
+    respostas: [
+      { text: "Pine trees", correto: false },
+      { text: "Cork oaks", correto: false },
+      { text: "Mediterranean scrubland", correto: true },
+      { text: "Olive trees", correto: false },
+    ],
+  },
+  {
+    pergunta: "Which historical site is located within the Arrábida mountains?",
+    respostas: [
+      { text: "Convento de Cristo", correto: false },
+      { text: "Convento da Arrábida", correto: true },
+      { text: "Convento de Mafra", correto: false },
+      { text: "Convento de Belém", correto: false },
+    ],
+  },
+  {
+    pergunta: "What is the Arrábida Natural Park best known for?",
+    respostas: [
+      { text: "Coastal cliffs and biodiversity", correto: true },
+      { text: "Desert landscapes", correto: false },
+      { text: "Snow sports", correto: false },
+      { text: "Volcanic activity", correto: false },
+    ],
+  },
+  {
+    pergunta: "Which popular outdoor activity is common in the Arrábida mountains?",
+    respostas: [
+      { text: "Surfing", correto: false },
+      { text: "Rock climbing", correto: true },
+      { text: "Skiing", correto: false },
+      { text: "Paragliding", correto: false },
+    ],
+  },
+  {
+    pergunta: "Which beach is renowned for its clear waters and is located in Arrábida?",
+    respostas: [
+      { text: "Praia da Rocha", correto: false },
+      { text: "Praia da Figueirinha", correto: true },
+      { text: "Praia de Carcavelos", correto: false },
+      { text: "Praia da Nazaré", correto: false },
+    ],
+  },
+  {
+    pergunta: "Which beach is famous for its view of the Troia Peninsula?",
+    respostas: [
+      { text: "Praia da Adraga", correto: false },
+      { text: "Praia da Ribeira", correto: false },
+      { text: "Praia de Galápos", correto: true },
+      { text: "Praia da Manta Rota", correto: false },
+    ],
+  },
+  {
+    pergunta: "What is a common activity at Praia dos Coelhos?",
+    respostas: [
+      { text: "Scuba diving", correto: true },
+      { text: "Snowboarding", correto: false },
+      { text: "Windsurfing", correto: false },
+      { text: "Fishing", correto: false },
+    ],
+  },
+  {
+    pergunta: "Which beach is known for its secluded and tranquil environment in Arrábida?",
+    respostas: [
+      { text: "Praia da Comporta", correto: false },
+      { text: "Praia de Albarquel", correto: true },
+      { text: "Praia do Guincho", correto: false },
+      { text: "Praia da Amoreira", correto: false },
+    ],
+  },
+  {
+    pergunta: "Which beach is part of the Arrábida Natural Park and has limited access?",
+    respostas: [
+      { text: "Praia da Rocha", correto: false },
+      { text: "Praia do Portinho da Arrábida", correto: true },
+      { text: "Praia da Foz do Arelho", correto: false },
+      { text: "Praia de São Pedro de Moel", correto: false },
+    ],
+  },
+  // True or false questions
+  {
+    pergunta: "Is Choco Frito a traditional dish from Setúbal?",
+    respostas: [
+      { text: "True", correto: true },
+      { text: "False", correto: false },
+    ],
+  },
+  {
+    pergunta: "Is Tortas de Azeitão a type of cheese?",
+    respostas: [
+      { text: "False", correto: true },
+      { text: "True", correto: false },
+    ],
+  },
+  {
+    pergunta: "Is Queijo de Azeitão a soft, creamy cheese made from cow's milk?",
+    respostas: [
+      { text: "False", correto: true },
+      { text: "True", correto: false },
+    ],
+  },
+  {
+    pergunta: "Is Moscatel de Setúbal a sweet, fortified wine made from Muscat grapes?",
+    respostas: [
+      { text: "True", correto: true },
+      { text: "False", correto: false },
+    ],
+  },
+  {
+    pergunta: "Is Sardinhas Assadas a common dish in Setúbal?",
+    respostas: [
+      { text: "True", correto: true },
+      { text: "False", correto: false },
+    ],
+  },
+];
+
+
+/* // Questions array containing the quiz questions and answers
 const questions = [
   {
     pergunta: "What is the main square of Setúbal called?",
@@ -153,4 +359,4 @@ const questions = [
       { text: "True", correto: true },
     ],
   },
-];
+]; */
