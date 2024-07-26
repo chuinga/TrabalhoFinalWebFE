81fa85aa12918f7feb019c432dcabd418d6ddfbb
chuinga <mfproencamartins@gmail.com>
2024-07-26 16:12:47 +0100

    feat: change increment variable and translate comments in main.js file

--- scripts/main.js
+++ scripts/main.js
@@ -1,32 +1,35 @@
 function currentYear() {
-    document.getElementById('current-year').textContent = new Date().getFullYear();
-  }
-  
-  document.addEventListener('DOMContentLoaded', function() {
-    currentYear();
-  });
+  document.getElementById("current-year").textContent =
+    new Date().getFullYear();
+}
 
-  const atualizaContagem = (elemento) => {
-    //Obter o valor a partir do atributo 'data-value' do elemento
-    const valor = parseInt(elemento.dataset.value); //parseInt --> produz um valor inteiro ditado pela interpretação do conteúdo
-    // Calcula o incremento a ser adicionado a cada intervalo de tempo
-    const incremento = Math.ceil(valor / 1000); //Math.ceil arredonda valor para cima
-    // Inicializar o valor inicial da contagem
-    let valorInicial = 0;
-    // Definição de uma função que será executada repetidamente a cada milissegundo
-    const aumentaContagem = setInterval(() => {
-      valorInicial += incremento;     // Adiciona o incremento ao valor inicial
-      if (valorInicial > valor) {    // Verifica se o valor inicial ultrapassou o valor desejado
-        elemento.textContent = `${valor}`; // Define o texto do elemento para o valor desejado 
-        clearInterval(aumentaContagem);// Para a execução repetida da função
-        return;
-      }
-      elemento.textContent = `${valorInicial}`; // Atualiza o texto do elemento para o valor atual da contagem
-    }, 1);                                // O intervalo de tempo é definido como 1 milissegundo
-  };
-  // Seleciona todos os elementos da página com a classe 'numero' e converte-os num array
-  const items = [...document.querySelectorAll('.number')];
-  // Para cada elemento selecionado, aplica a função 'atualizaContagem'
-  items.forEach((item) => {
-    atualizaContagem(item);
-  });
+document.addEventListener("DOMContentLoaded", function () {
+  currentYear();
+});
+
+const counterUpdate = (element) => {
+  //Get the value from the 'data-value' attribute of the element
+  const value = parseInt(element.dataset.value); //parseInt --> produces an integer value based on the interpretation of the content
+  // Calculate the increment to be added at each time interval
+  const increment = Math.ceil(value / 10000); //Math.ceil rounds value up
+  // Initialize the initial value of the count
+  let initialValue = 0;
+  // Define a function that will be executed repeatedly every millisecond
+  const incrementCounter = setInterval(() => {
+    initialValue += increment; // Add the increment to the initial value
+    if (initialValue > value) {
+      // Check if the initial value has exceeded the desired value
+      element.textContent = `${value}`; // Set the element's text to the desired value
+      clearInterval(incrementCounter); // Stop the repeated execution of the function
+      return;
+    }
+    element.textContent = `${initialValue}`; // Update the element's text to the current value of the count
+  }, 1); // The time interval is set to 1 millisecond
+};
+// Select all elements on the page with the class 'number' and convert them to an array
+const items = [...document.querySelectorAll(".number")];
+// For each selected element, apply the 'counterUpdate' function
+
+items.forEach((item) => {
+  counterUpdate(item);
+});
